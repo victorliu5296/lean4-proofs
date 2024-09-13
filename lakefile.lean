30a6433eadd45240339f488fb914765d228d30d4
- <victorliu5296@outlook.com>
2024-09-12 20:51:23 -0400

    Initial commit

---
+++ lakefile.lean
@@ -0,0 +1,15 @@
+import Lake
+open Lake DSL
+
+package "lean4_proofs" where
+  version := v!"0.1.0"
+  keywords := #["math"]
+  leanOptions := #[
+    ⟨`pp.unicode.fun, true⟩ -- pretty-prints `fun a ↦ b`
+  ]
+
+require "leanprover-community" / "mathlib"
+
+@[default_target]
+lean_lib «Lean4Proofs» where
+  -- add any library configuration options here
